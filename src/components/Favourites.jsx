f9b88d16a7406bc3381e1f826d566fe532a19b35
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-09-13 16:58:27 +0200

    loading in favourites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -5,6 +5,13 @@ import Job from "./Job";
 
 const Favourites = () => {
     const favourites = useSelector((state) => state.favourite.list);
+    const loading = false;
+    // let loadind
+    //     useEffect(() => {
+    //         setTimeout(() => {
+    //             loading: false;
+    //         }, 200);
+    //     }, []);
 
     return (
         <Container>
@@ -13,11 +20,18 @@ const Favourites = () => {
                     <h1>Favourite Jobs</h1>
                 </Col>
 
-                <Col xs={10} className="mx-auto mb-5">
-                    {favourites.map((company) => (
-                        <Job key={company._id} data={company} />
-                    ))}
-                </Col>
+                {loading ? (
+                    <Col xs={10} className="mx-auto mt-5 text-center">
+                        {" "}
+                        <Spinner animation="grow" variant="primary" />
+                    </Col>
+                ) : (
+                    <Col xs={10} className="mx-auto mb-5">
+                        {favourites.map((company) => (
+                            <Job key={company._id} data={company} />
+                        ))}
+                    </Col>
+                )}
             </Row>
         </Container>
     );
